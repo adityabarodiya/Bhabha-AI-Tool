4d7b6a2cbbdb6be6fc4018953f32b73b93e9ecf0
Aditya Barodiya <98759220+adityabarodiya@users.noreply.github.com>
2024-04-08 21:02:13 +0530

    Update route.ts

--- src/app/api/test/result/route.ts
+++ src/app/api/test/result/route.ts
@@ -2,6 +2,8 @@ import { NextResponse, NextRequest } from "next/server";
 
 import programmingQuestions from "@/questionAns";
 
+//
+
 
 export async function GET(request : NextRequest){
     return NextResponse.json(programmingQuestions)
